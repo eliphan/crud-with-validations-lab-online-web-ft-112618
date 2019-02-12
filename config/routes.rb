406f26adae7ce3710b1fdf606961080b1b215f36
Elise Phan <aliceconnic@gmail.com>
2019-02-12 19:35:11 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -6,3 +6,12 @@ Rails.application.routes.draw do
   
 end
 
+  get '/songs', to: 'songs#index'
+  get '/songs/new', to: 'songs#new', as: 'new_song'
+  post '/songs', to: 'songs#create'
+  get '/songs/:id', to: 'songs#show', as: 'song'
+  get '/songs/:id/edit', to: 'songs#edit', as: 'edit_song'
+  patch '/songs/:id', to: 'songs#update'
+  delete '/songs/:id', to: 'songs#destroy'
+
+  root 'songs#index'
