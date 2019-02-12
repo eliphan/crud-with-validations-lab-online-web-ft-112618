d87bce70b1e7eb7ec9427b63f5f0b2a38c028a1c
Elise Phan <aliceconnic@gmail.com>
2019-02-12 19:32:21 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,6 +8,7 @@ class SongsController < ApplicationController
   end
   
   def create
+    binding.pry
     @song = Song.new(song_params)       
     if @song.save
       redirect_to song_path(@song)
