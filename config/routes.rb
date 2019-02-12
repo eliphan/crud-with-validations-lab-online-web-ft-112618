595888dbaea55ca74038a2a8915f474b8a65f65d
Elise Phan <aliceconnic@gmail.com>
2019-02-12 19:04:11 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :songs, only: [:index,:new, :create, :show]
   get 'songs/:id/edit', to: 'songs#edit', as: :edit_song
   patch 'songs/:id', to: 'songs#update'
-  delete '/songs/:id', to: 'song#destroy'
+  delete '/songs/:id', to: 'songs#destroy'
   
 end
